e7dd9ffe345f8a32fa188e673e0663e0af22875b
Anton Krug <antonkrug@gmail.com>
2016-12-31 20:21:37 +0000

    Unplug TV card and restart network

---
+++ odpoj_tv_kartu.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#/sbin/lspcmcia 
+#pccardctl status
+#/sbin/pccardctl -v ls
+#/sbin/pccardctl eject
+#/sbin/pccardctl -v ls
+#pccardctl status
+#/sbin/lspcmcia
+
+
+
+#rmmod -fv saa7134_alsa
+#modprobe -rfv saa7134_alsa
+#rmmod saa7134_dvb
+rmmod saa7134
